1ca6cc99b129ddf17b5ca6068fea244c5ac40afb
noahehall <noahehall@users.noreply.github.com>
2022-12-20 00:36:58 -0700

    add check before setting instance

--- modules/elastic-ip/ip.tf
+++ modules/elastic-ip/ip.tf
@@ -4,7 +4,8 @@ resource "aws_eip" "ip" {
   vpc = true
 
   network_interface = var.ni.id != "" ? var.ni.id : null
-  instance = var.ec2.id != "" ? var.ec2.id : null
+  # prevent specifying both, with a preference for network_interface
+  instance = var.ec2.id != "" && var.ni.id == "" ? var.ec2.id : null
 
   tags = merge(var.tags, {
     Name         = var.ip.name
